8a13793330b823b2e5a349af3bcf591f37701508
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-06 16:27:26 +0530

    ./

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
 const winston = require('winston');
 const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db'); 
+const connectDB = require('./config/db');
 const documentRoutes = require('./routes/documentRoutes');
 const authRoutes = require('./routes/authRoutes');
 const auditRoutes = require('./routes/auditRoutes');
@@ -16,8 +18,8 @@ connectDB();
 
 app.use(helmet());
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173', 
-    credentials: true 
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true
 }));
 
 
@@ -32,17 +34,17 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
-    new winston.transports.Console({ format: winston.format.simple() }) 
+    new winston.transports.Console({ format: winston.format.simple() })
   ],
 });
 
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 
 app.get('/', (req, res) => {
-    res.status(200).json({ 
-        message: 'Secure Document Sharing API is running', 
-        timestamp: new Date() 
-    });
+  res.status(200).json({
+    message: 'Secure Document Sharing API is running',
+    timestamp: new Date()
+  });
 });
 
 app.use('/api/auth', authRoutes);
@@ -51,5 +53,5 @@ app.use('/api/audit', auditRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    logger.info(`[Server] Running on port ${PORT}`);
+  logger.info(`[Server] Running on port ${PORT}`);
 });
