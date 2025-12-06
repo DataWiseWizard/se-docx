931a563aaf4fccf5149c6346b1feebcf39dce1db
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-06 16:38:09 +0530

    Fixed minor server issuess

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/DashBoard';
+import Dashboard from './pages/Dashboard';
 import './App.css'
 
 // const Dashboard = () => (
