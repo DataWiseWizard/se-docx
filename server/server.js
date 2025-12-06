09637f3be87ab5ad39aac2cb5fc5ee36fb949965
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-06 16:29:11 +0530

    Fixed minor server issue

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
