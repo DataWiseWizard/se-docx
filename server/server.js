b32caedad00e390ef95ba9acfa516096cd4f4a1d
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-16 20:12:39 +0530

    ./

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.use('/api/folders', folderRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
