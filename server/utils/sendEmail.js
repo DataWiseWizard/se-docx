0bb54a75c24f432ac2b628fd24b78edcf350c52b
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-16 19:46:21 +0530

    ./././

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,7 @@ const { Resend } = require('resend');
 
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM_EMAIL = 'Se-Docx <noreply@vault.pinch-your-pins.site>';
+const FROM_EMAIL = 'Se-Docx <noreply@pinch-your-pins.site>';
 
 const sendEmail = async (options) => {
     try {
