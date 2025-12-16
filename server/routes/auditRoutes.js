9b224c40846bcc91e1aa34283e674de651322f50
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-16 14:56:51 +0530

    ./

--- server/routes/auditRoutes.js
+++ server/routes/auditRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { getMyLogs } = require('../controllers/auditController');
 const { protect } = require('../middleware/authMiddleware');
-
 const router = express.Router();
 
 router.get('/', protect, getMyLogs);
