30c7073753b3c3a58e5fdf1fad44c42650b3a759
Rudraksha <rudrakshakumbhkar@gmail.com>
2025-12-25 14:54:07 +0530

    ./././././././

--- server/models/User.js
+++ server/models/User.js
@@ -54,17 +54,15 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
     if (!this.password || !this.isModified('password')) {
-        return next();
+        return;
     }
-
     try {
         const salt = await bcrypt.genSalt(12);
         this.password = await bcrypt.hash(this.password, salt);
-        next();
     } catch (error) {
-        next(error);
+        throw new Error(error);
     }
 });
 
